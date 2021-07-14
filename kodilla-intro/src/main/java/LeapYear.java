c4806d7d53b8a6fa11fa5350288574ed4882e7b0
Krzysztof Nowakowski <kn90@op.pl>
2021-07-14 14:48:54 +0200

    Initial changes

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,14 +1,12 @@
 public class LeapYear {
     public static void main(String[] args) {
-        int year = 1900;
-        if (year % 4 > 0 ) {
-            System.out.println("Rok jest nieprzestępny");
-        }if (year % 4 > 0 && year % 100 >0 ){
-                System.out.println("Rok jest nieprzestępny");}
-                if (year % 4 > 0 && year % 100 > 0 && year % 400 >0 ){
-                    System.out.println("rok jest nieprzestępny");}
-                    else {
-                        System.out.println("rok jest przestępny");
-            }
-            }
+        int year = 2021;
+        if (year % 4 == 0 && year % 100 !=0) {
+            System.out.println("Rok jest przestępny");
+        }   else if(year % 400 == 0){
+            System.out.println("Rok jest przestępny");
         }
+        else {
+                        System.out.println("rok jest nieprzestępny");}
+            }
+            }
--- kodilla-intro/src/test/java/Test1.java
+++ kodilla-intro/src/test/java/Test1.java
@@ -1,9 +1,9 @@
 public class Test1 {
-    int i = 0;
-while(i <=10) {
-
-    {
-        System.out.println(i);
-        i++;
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
     }
-}}
+}
