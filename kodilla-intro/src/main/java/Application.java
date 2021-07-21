79a7c0b0ccf405546c0ea6ad0cf4404f4ba934ff
Krzysztof Nowakowski <kn90@op.pl>
2021-07-21 14:39:06 +0200

    initial changes

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -8,11 +8,9 @@ public class Application {
 
     public static void main(String[] args) {
         Application adam = new Application(40.5,178);
-        double age = 40.5;
-        double height = 178;
 
         if (adam != null) {
-            if (age > 30 && height > 160) {
+            if (adam.age > 30 && adam.height > 160) {
                 System.out.println("User is older than 30 and higher than 160cm");
             } else {
                 System.out.println("User is 30 (or younger) or 160cm (or shorter)");
