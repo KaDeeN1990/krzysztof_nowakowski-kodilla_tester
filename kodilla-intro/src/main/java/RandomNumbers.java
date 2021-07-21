b9c81ec812323d051a52372f2f6f1f7e0c980a69
Krzysztof Nowakowski <kn90@op.pl>
2021-07-21 13:47:10 +0200

    initial changes

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,6 +1,18 @@
+import java.util.Random;
 public class RandomNumbers {
     public static void main(String[] args) {
-    int liczba;
-
+    Random r = new Random();
+    int min = 30;
+    int max = 0;
+    int sum = 0;
+    while(sum < 5000) {
+        int a = r.nextInt(31);
+        if (a>max) max = a;
+        if (a<min) min = a;
+        sum = sum + a;
+    }
+    System.out.println(sum);
+    System.out.println("Najwieksza liczba to" + " " + max + ",");
+    System.out.println("Najmniejsza liczba to" + " " + min + ".");
     }
 }
