38c37654765c99eb4b288a654ffc5c51f162a46b
Krzysztof Nowakowski <kn90@op.pl>
2021-08-06 10:45:14 +0200

    initial changes

--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -6,10 +6,10 @@ import java.util.Set;
 public class StampsApplication {
     public static void main(String[] args) {
         Set<Stamp> stamps = new HashSet<>();
-        stamps.add(new Stamp("Battle", "25cm x 25cm", true));
-        stamps.add(new Stamp("Love", "30cm x 30cm", false));
-        stamps.add(new Stamp("Landscape", "20cm x 20cm", true));
-        stamps.add(new Stamp("Love", "30cm x 30cm", false));
+        stamps.add(new Stamp("Battle", "25mm x 25mm", true));
+        stamps.add(new Stamp("Love", "30mm x 30mm", false));
+        stamps.add(new Stamp("Landscape", "20mm x 20mm", true));
+        stamps.add(new Stamp("Love", "30mm x 30mm", false));
         System.out.println(stamps.size());
         for (Stamp stamp : stamps)
             System.out.println(stamp);
