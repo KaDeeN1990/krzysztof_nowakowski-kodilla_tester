1eb52c08c52a006559dae482df7847d798235d8f
Krzysztof Nowakowski <kn90@op.pl>
2021-08-09 12:24:59 +0200

    initial changes

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/BookHacked.java
@@ -0,0 +1,12 @@
+package com.kodilla.collections.adv.immutable;
+
+public class BookHacked{
+    private String realTitle;
+
+
+
+    public void modifyTitle(String newTitle) {
+        realTitle = newTitle;
+    }
+
+}
