8161beb3ed20a981e9268af46377f64573a74d74
Krzysztof Nowakowski <kn90@op.pl>
2021-08-18 09:52:36 +0200

    initial changes

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -1,9 +1,6 @@
 package com.kodilla.exception.homework;
 
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Warehouse {
 
     public static Order addOrder(String number){
@@ -11,10 +8,6 @@ public class Warehouse {
         return order;
     }
     public void getOrder(String number){
-        List<String> ordered = WarehouseApp.getNumber()
-                .stream()
-                .map(u->u.getNumber())
-                .collect(Collectors.toList());
-        System.out.println(ordered);
+
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WarehouseApp {
-    public static List<Order> getNumber() {
+    public static void main(String[] args) {
         List<Order> orders = new ArrayList<>();
         orders.add(Warehouse.addOrder("101"));
         orders.add(Warehouse.addOrder("201"));
         orders.add(Warehouse.addOrder("301"));
-        return orders;
+        System.out.println(orders);
     }
 }
